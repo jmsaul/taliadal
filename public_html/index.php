55c0309db398f09b86233e8be69a6573d14ee720
Jeff Saul <scaleup13@gmail.com>
2016-05-17 23:37:37 -0600

    started setting up index.php

---
+++ public_html/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<!--------------------------------------------------------------->
+	<!--  Tiliadal STEM Education Consulting Homepage              -->
+	<!--  																			-->
+	<!--  created by webmansaul@gmail.com     May 2016					-->
+	<!--------------------------------------------------------------->
+
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<meta name="viewport" content="width-device-width, initial-scale-1"/>
+
+		<title>Tiliadal STEM Ed</title>
+
+	</head>
+
+	<body>
+		<h1>Tiliadal STEM Education Consulting</h1>
+	</body>
+</html>
