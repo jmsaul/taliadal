a173d77accd09a3520b7fc57d467f0114c534552
Jeff Saul <scaleup13@gmail.com>
2016-08-13 22:35:06 -0600

    updated nav bar on index1.php file

---
+++ public_html/Index1.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags  		-->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"
+
+		<!-- Bootstrap latest compiled and minified CSS		-->
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"
+				integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+				crossorigin="anonymous">
+
+		<!-- Optional Bootstrap Theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- [if it IE 9]>
+			<script src="//css.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="//css.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<! [endif]-->
+
+		<!-- jQuery (required for Bootstrap's JS plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+		<!-- Bootstrap latest compiled and minified Javascript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+				  integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+				  crossorigin="anonymous"></script>
+
+		<title>Tiliadal STEM Ed</title>
+	</head>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<nav class="navbar navbar-inverse">
+				<!-- logo and mobile toggle button get grouped together for better mobile display -->
+				<div class="navbar-header white-text0">
+					<!-- this is the mobile menu button -->
+					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+						<span class="sr-only">main menu</span>
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
+					</button>
+					<span>
+						<a class="navbar-brand center-text" href="#">Tiliadal STEM Education Conulting</a>
+					</span>
+					<div class="clearfix"></div>
+					<div class="white-text1">Helping faulty, programs, and deaparments implement STEM Education Projects</div>
+				</div>
+
+
+				<!-- here are your main nav links, grouped for toggling -->
+				<div class="collapse navbar-collapse" id="main-menu">
+					<u1 class="nav navbar-nav navbar-right"
+					</u1>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="/pages/logi-gaming.php">About Us</a></li>
+					<li><a href="/pages/community-debate.php">Services</a></li>
+					<li><a href="/pages/services.php">Projects</a></li>
+					<li><a href="/pages/contact,php">Resources</a></li>
+					<li><a href="/pages/contact,php">Blog</a></li>
+				</div>
+			</nav>
+
+			<main class="main-fomrat">
+				<div class="container">
+					<h4>Professional Statement</h4>
+					<p>In my previous career, I focused on the teaching and learning of physics, astronomy, and mathematics at an introductory undergraduate level, most recently at the University of New Mexico.  I earned a Ph.D. in Physics in Physics Education Research.  I am now looking for a new career that lets me apply my knowledge and technical skills.  I enjoy coding and had coded applications both as an undergraduate and graduate student.  I am currently updating my skills to pursue a career in web application design and development.  I like the challenge of interesting problems and finding practical solutions.
+					</p>
+					<a href="about-me.php"><h4>To learn more about me</h4></a>
+				</div>
+				<hr>
+
+				<div class="container">
+					<div class="text-center">
+
+						<div class="row">
+							<div class="col-md-6">
+								<div>
+									<h3>LogiGaming</h3>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+								</div>
+
+							</div>
+
+							<div class="col-md-6">
+								<div>
+									<h3>Community Debate</h3>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								</div>
+
+							</div>
+
+
+
+						</div>
+					</div>
+				</div>
+
+				<br>
+				<hr>
+				<div class="container">
+					<div class="col-md-6">
+						<h4>Contact Information:</h4>
+						<ul class="contact-info">
+							<li><a href="//bootcamp-coders.cnm.edu/~jsaul4/saul-pwpsite">Personal Website</a></li>
+							<li><a href="//github.com/jmsaul">Github Repository</a></li>
+							<li><a href="mailto:webmansaul@gmail.com">webmansaul@gmail.com</a></li>
+							<li>Phone: 954-665-5373</li>
+							<li><a
+									href="//www.linkedin.com/profile/view?id=AAMAAAZJ_f0BjhDGc9euxLgYOwz_kfWCqZ2XDlU&trk=hp-identity-name">Linked
+									in</a></li>
+							<li><a href="https://twitter.com/NMmodeling">Twitter</a></li>
+						</ul>
+					</div>
+
+					<div class="col-md-6">
+						<div>LogiGaming</div>
+						<div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+
+					</div>
+
+			</main>
+		</div>
+
+
+		<footer class="footer">
+			<div class="container">
+				<p class="text-muted">Copyright 2016 Chris Jaramillo
+			<span class="pull-right">
+				<!--	This will send an email to trailquailabq@gmail.com		-->
+				<a class="btn btn-primary" href="mailto:C.Jaramillo@live.com"
+					role="button">Email LogiGaming</a>
+			</span>
+				</p>
+			</div>
+		</footer>
+
+	</body>
+</html>
