42236b542a0e2a6dbfb8bd3f2fff96a0e0f5fd8a
Jeff Saul <scaleup13@gmail.com>
2016-08-17 00:22:24 -0600

    background adjustments including background color and addition of website construction warning

---
+++ public_html/index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<!--------------------------------------------------------------->
+	<!--  Tiliadal STEM Education Consulting Homepage              -->
+	<!--  																			-->
+	<!--  created by webmansaul@gmail.com     May 2016					-->
+	<!--------------------------------------------------------------->
+
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<meta name="viewport" content="width-device-width, initial-scale-1"/>
+
+		<title>Tiliadal STEM Ed</title>
+
+	</head>
+
+	<body>
+		<h1>Tiliadal STEM Education Consulting</h1>
+		<p>Website currently under construction - expected update 8/15/2016</p>
+	</body>
+</html>
