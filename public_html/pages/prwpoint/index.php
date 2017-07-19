b92372384ebbdacd32ec07fdc6db7d2950594547
jmsaul <scaleup13@gmail.com>
2017-07-18 19:14:20 -0600

    Added index.php for presentaton folder / removed errors4new readout

---
+++ public_html/pages/prwpoint/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Stem Ed Solutions</title>
+
+		<!-- CDN Bootstrap Libraries -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!--  End of CDN Bootstrap Libraries -->
+
+		<!-- Load CSS style sheets -->
+		<!--  resets CSS defaults  -->
+		<link rel="stylesheet" href="../../css/reset.css">
+		<!--  CSS styling for this website  -->
+		<link rel="stylesheet" href="../../css/global.css">
+
+		<!-- media query set-up -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=true;">
+	</head>
+
+	<body>
+		<header>
+			<!-- This is the main navigation bar -->
+			<!-- Next Steps:  Would like to put company name into 2 rows and realign the navigation links so that they are verically centered -->
+			<nav>
+				<div class="container nav-bckgrnd">
+					<!--  Note that this <h1> text does not appear on screeen -->
+					<h1 id="brand">Tiliadal STEM Education Solutions</h1>
+					<br/>
+					<h2 id="company">Tiliadal STEM Education Solutions</h2>
+					<ul>
+						<!--  Main Navigation links -->
+						<li><a href="../../index.php">Home</a></li>
+						<li><a href="index.php">Projects</a></li>
+						<li><a href="../pwrpoint/index.php">Presentatons</a></li>
+						<li><a href="../workshops/index.php">Workshops</a></li>
+						<li><a href="../papers/index.php">Publications</a></li>
+						<li><a href="../services/index.php">Services</a></li>
+						<li><a href="../team/index.php">Meet the Team</a></li>
+						<li><a href="mailto:jsaul@tiliadal.com?subject=Contact Tiliadal STEM Ed">Contact us</a></li>
+					</ul>
+				</div>
+				<!-- Company Slogan - may need further work -->
+				<h3>Personalized Solutions to Help You Grow Your Educational Ideas into Reality</h3>
+			</nav>
+			<!-- End of Navigatoin bar in header section -->
+		</header>
+
+		<!-- Main Content Section of the Webpage ** list services for potential clients -->
+		<section class="main-content">
+			<div class="container">
+				<h2 id="pageHeader">Current Projects</h2>
+			<!-- ********************************** -->
+			<!-- List of Projects						 -->
+			<!-- ********************************** -->
+
+
+		<!--  Emd of main contect Section -->
+		</div>
+		<!-- Flourish separator begins here -->
+		<section class="separator">
+			<img src="../../img/flourish1-.gif" class="flourish" alt="flourish divider" />
+		</section>
+		<!-- Flourish separator emds here -->
+
+		<!--  Hero Section with company mission statement -->
+		<section id="hero">
+			<!-- inset hero image -->
+			<!--  <img src="images/zebra.jpg" alt="Zebra"/>  -->
+			<span class="motto">We are an interdisciplinary team of Science Educators, Cognitive and Learning Scientists, and STEM Education Researchers dedicated to improving STEM teaching, development, and education research at all education levels.</span>
+		</section>
+
+		<section id="social">
+			<div class="container">
+				<h3>Coming soon to a social media platform near you.</h3>
+				<!--    Commenting out socal media links for btomt
+				<ul>
+					<li><img src="img/facebook.svg" alt="Facebook"/></li>
+					<li><img src="img/twitter.svg" alt="Twitter"/></li>
+					<li><img src="img/instagram.svg" alt="Instagram"/></li>
+					<li><img src="img/linkedin.svg" alt="LinkedIn"/></li>
+				</ul> -->
+			</div>
+		</section>
+		<footer>
+			<p>&copy; 2017 Webmansaul</p>
+		</footer>
+	</body>
+</html>
