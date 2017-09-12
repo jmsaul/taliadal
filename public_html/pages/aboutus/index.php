b788f315090a63d9241f47bd725cff549b242a41
Jeff Saul <scaleup13@gmail.com>
2017-09-12 16:24:38 -0600

    uploaded text for About Us page, created

---
+++ public_html/pages/aboutus/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>STEM Ed Solutions Team</title>
+
+		<!-- CDN Bootstrap Libraries -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!--  End of CDN Bootstrap Libraries -->
+
+		<!-- Load CSS style sheets -->
+		<!--  resets CSS defaults  -->
+		<link rel="stylesheet" href="../../css/reset.css">
+		<!--  CSS styling for this website  -->
+		<link rel="stylesheet" href="../../css/global.css">
+
+		<!-- media query set-up -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=true;">
+	</head>
+
+	<body>
+		<header>
+			<!-- This is the main navigation bar -->
+			<!-- Next Steps:  Would like to put company name into 2 rows and realign the navigation links so that they are verically centered -->
+			<nav>
+				<div class="container nav-bckgrnd">
+					<!--  Note that this <h1> text does not appear on screeen -->
+					<h1 id="brand">Tiliadal STEM Education Solutions</h1>
+					<br/>
+					<h2 id="company">Tiliadal STEM Education Solutions</h2>
+					<ul>
+						<!--  Main Navigation links -->
+						<li><a href="../../index.php">Home</a></li>
+						<li><a href="../aboutus/index.php">About Us</a></li>
+						<li><a href="../workshops/index.php">Workshops Offered</a></li>
+						<li><a href="../services/index.php">Services Offered</a></li>
+						<li><a href="../team/index.php">Meet the Team</a></li>
+						<li><a href="mailto:jsaul@stemedsolutions.com,rlindell@tiliadal.com?subject=Contact Tiliadal STEM Ed Solutions">Contact us</a></li>
+					</ul>
+				</div>
+				<!-- Company Slogan - may need further work -->
+				<h3>Personalized Solutions to Help You Grow Your Educational Ideas into Reality</h3>
+			</nav>
+			<!-- End of Navigatoin bar in header section -->
+		</header>
+
+		<!-- Main Content Section of the Webpage ** list services for potential clients -->
+		<section class="pages-content">
+			<div class="container">
+				<h2 id="pageHeader">About Us</h2>
+
+
+			<!-- List of Projects						 -->
+				<div class="left-justify">
+					<p>Tiliadal (Tilleeadahl) STEM Education Solutions, founded in 2014, is a consulting company dedicated to improving STEM education by working in partnership with academic institutions, professional societies and individual faculty to promote change in STEM Education.  We do this through this by working closely with our clients to design custom solutions to reach their education goals.  Our team specializes in developing and designing methods/ paths toward incorporating change into STEM higher education.  We have worked closely with individual faculty members to develop their ideas into a product to help students across the US, as well as working with individual universities to design and deliver professional development workshops for their entire faculty.
+						.</p>
+					<p>Tiliadal’s symbol is a full grown tree representing stability, structure and wisdom as well as guidance and support provided by our company.  The name is a play on the founder, Rebecca Lindell’s last name, which literally means “valley of the Linden trees” in Swedish. “Tilia” is the family name of over 30 species of trees including the Linden tree, while dal also refers to valley in Swedish.</p>
+					<a href="rl-vita">Link to Curriculum Vitae</a>
+					<br/>
+					<br/>
+				</div>
+
+
+
+
+				<!-- ********************************** -->
+			<!-- ********************************** -->
+
+
+		<!--  Emd of main content Section -->
+		</div>
+		<!-- Flourish separator begins here -->
+		<section class="separator">
+			<img src="../../img/flourish1-.gif" class="flourish" alt="flourish divider" />
+		</section>
+		<!-- Flourish separator emds here -->
+
+		<!--  Hero Section with company mission statement -->
+		<section id="hero">
+			<!-- inset hero image -->
+			<!--  <img src="images/zebra.jpg" alt="Zebra"/>  -->
+			<span class="motto">We are an interdisciplinary team of Science Educators, Cognitive and Learning Scientists, and STEM Education Researchers dedicated to improving STEM teaching, development, and education research at all education levels.</span>
+		</section>
+
+		<section id="social">
+			<div class="container">
+				<h3>Coming soon to a social media platform near you.</h3>
+				<!--    Commenting out socal media links for btomt
+				<ul>
+					<li><img src="img/facebook.svg" alt="Facebook"/></li>
+					<li><img src="img/twitter.svg" alt="Twitter"/></li>
+					<li><img src="img/instagram.svg" alt="Instagram"/></li>
+					<li><img src="img/linkedin.svg" alt="LinkedIn"/></li>
+				</ul> -->
+			</div>
+		</section>
+		<footer>
+			<p>&copy; 2017 Webmansaul</p>
+		</footer>
+	</body>
+</html>
